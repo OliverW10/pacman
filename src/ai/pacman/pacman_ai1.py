50aaafd1d56ff6f14954f3c5a57fe88cb847979d
oliverw10 <oliver@warrickfamily.com>
2022-06-28 12:01:56 +1000

    added collect.py, train.py and level_mapper.py

--- src/ai/pacman/pacman_ai1.py
+++ src/ai/pacman/pacman_ai1.py
@@ -9,10 +9,11 @@ from game.ghosts import BaseGhostSystem, GhostMode
 # pacman that picks the path that has the highest score
 # has no concept of ghosts and if there are no pellets near it it has a panic attack
 class GreedyPacman(BasePacman):
-    def __init__(self, x, y):
+    def __init__(self, x, y, depth=8):
         super().__init__(x, y)
         self.wanted_dir = Direction.NONE
         self.path = []
+        self.depth = depth
 
     @property
     def cornercut(self) -> float:
@@ -23,10 +24,9 @@ class GreedyPacman(BasePacman):
         self, dt: float, level_map: List[List[Tile]], ghost_system: BaseGhostSystem
     ):
         super().step(dt, level_map)
-        depth = 8
-        pacman_tree = create_tree(level_map, (self.x, self.y), depth)
+        pacman_tree = create_tree(level_map, (self.x, self.y), self.depth)
         ghost_trees = [
-            create_tree(level_map, (ghost.x, ghost.y), depth, ghost.direction)
+            create_tree(level_map, (ghost.x, ghost.y), self.depth, ghost.direction)
             for ghost in ghost_system.ghosts
         ]
         self.path = get_path_from_tree(
@@ -68,15 +68,16 @@ class GreedyPacman(BasePacman):
         # step through each node in path
         while cur_node.parent:
             # check each ghost tree
-            for ghost_tree in ghost_trees:
-                # check each possibility the ghost could be at
-                for node in ghost_tree[layer]:
-                    # if its eaten us
-                    if node.pos == cur_node.pos or node.pos == last_node.pos:
-                        if eatable_for > 0:
-                            extra_score += 100
-                        else:
-                            return cur_node.score - 1000
+            for ghost_idx, ghost_tree in enumerate(ghost_trees):
+                if ghost_system.ghosts[ghost_idx].dead == False:
+                    # check each possibility the ghost could be at
+                    for node in ghost_tree[layer]:
+                        # if its eaten us
+                        if node.pos == cur_node.pos or node.pos == last_node.pos:
+                            if eatable_for > 0:
+                                extra_score += 100
+                            else:
+                                return cur_node.score - 1000
 
             if level_map[cur_node.pos[1]][cur_node.pos[0]] is Tile.SUPER_PELLET:
                 eatable_for += 10
@@ -85,3 +86,27 @@ class GreedyPacman(BasePacman):
             layer -= 1
             cur_node = pacman_tree[layer][cur_node.parent]
         return end_node.score + extra_score
+    
+    # estimates how 'good' each direction to go in is
+    def get_direction_weights(self, level_map, ghost_system) -> Tuple[float, float, float, float]:
+        depth = 8
+        pacman_tree = create_tree(level_map, (self.x, self.y), depth)
+        ghost_trees = [
+            create_tree(level_map, (ghost.x, ghost.y), depth, ghost.direction)
+            for ghost in ghost_system.ghosts
+        ]
+        # get all paths
+        pacman_tree[-1].sort(key=lambda x:self.evaluate_path(pacman_tree, x, level_map, ghost_trees, ghost_system))
+        paths = [get_path_from_tree(pacman_tree, end_node) for end_node in pacman_tree[-1][:3]]
+        direction_weights = [0, 0, 0, 0]
+        for idx, path in enumerate(paths):
+            d = path[1].direction
+            if d is Direction.RIGHT:
+                direction_weights[0] += 1/(idx+1)
+            if d is Direction.LEFT:
+                direction_weights[1] += 1/(idx+1)
+            if d is Direction.UP:
+                direction_weights[2] += 1/(idx+1)
+            if d is Direction.DOWN:
+                direction_weights[3] += 1/(idx+1)
+        return direction_weights
--- src/train.py
+++ src/train.py
@@ -1,8 +1,28 @@
+import math
 from ai.ghosts.ai1 import PredictGhostSystem
 from ai.ghosts.ai3 import AStarGhostSystem
 from game.ghosts import BaseGhostSystem, RandomGhostSystem
 from ai.pacman.pacman_random import RandomPacman
 from game.game import Game
-from game.level import classic_map
+from game.level import classic_map, classic_map_ghost, classic_map_pacman
+import tensorflow.keras as keras
 import tensorflow as tf
+from ai.level_mapper import to_pacman_relative_inputs, view_size
 
+# just guessing what will work, should experiment later
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(5, view_size, view_size)),
+    tf.keras.layers.Dense(math.floor(view_size*view_size), activation='relu'),
+    tf.keras.layers.Dense(math.floor(view_size*view_size)*0.1, activation='relu'),
+    tf.keras.layers.Dense(4)
+])
+
+if __name__ == "__main__":
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+    ghosts = BaseGhostSystem(classic_map_ghost)
+    pacman = RandomPacman(*classic_map_pacman)
+    to_pacman_relative_inputs(classic_map, ghosts, pacman, view_size)
